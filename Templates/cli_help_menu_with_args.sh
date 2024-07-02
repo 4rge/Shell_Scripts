cdf8263d7ea00635848fad346e744902358d1c5b
4rge <93495092+4rge@users.noreply.github.com>
2024-07-02 00:23:17 -0700

    Update cli_help_menu_with_args.sh

--- Templates/cli_help_menu_with_args.sh
+++ Templates/cli_help_menu_with_args.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env/ bash
 
+Black='\033[0;30m'
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+White='\033[0;37m'
+Neutral='\033[0m'
+Color1="$Purple"
+Color2="$Green"
+
+banner() {
+    msg="# $* #"
+    edge=$(echo "$msg" | sed 's/./#/g')
+    printf "$Color1$edge$Neutral\n"
+    echo "$msg"
+    printf "$Color2$edge$Neutral\n"
+}
+
 Help() {
    # Display Help
-   echo "Add description of the script functions here."
    echo
-   echo "Syntax: scriptTemplate [-g|h|v|V]"
-   echo "options:"
-   echo "g     Print the GPL license notification."
-   echo "h     Print this Help."
-   echo "v     Verbose mode."
-   echo "V     Print software version and exit."
+   banner "Add description of the script functions here."
+   echo
+   printf $Color1"Syntax:$Neutral scriptTemplate [-g|h|v|V]\n"
+   echo
+   banner "Options:"
+   echo
+   printf $Color1"-g$Neutral)     Print the GPL license notification.\n"
+   printf $Color2"-h$Neutral)     Print this Help.\n"
+   printf $Color1"-v$Neutral)     Verbose mode.\n"
+   printf $Color2"-V$Neutral)     Print software version and exit.\n"
    echo
 }
 
