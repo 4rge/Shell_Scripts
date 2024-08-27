81094d3ab642a08089c89fb6d3ffaf52839ee142
4rge <93495092+4rge@users.noreply.github.com>
2024-08-27 11:31:08 -0700

    Update mbspers.sh

--- Dump/mbspers.sh
+++ Dump/mbspers.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-"echo bitrate: $(/sbin/iw wlp1s0 station dump | perl -lne "print if /tx bitrate/ || /rx bitrate/ ..  s/^\s+|\s+$//g"
+echo $(/sbin/iw wlp1s0 station dump | perl -lne 'print if /tx bitrate/ || /rx bitrate/ ..  s/^\s+|\s+$//g')
