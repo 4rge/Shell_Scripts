74670eb6635b77d3f4fefe9e9d29acca2394dc39
4rge/michina <93495092+4rge@users.noreply.github.com>
2024-12-29 16:20:38 -0800

    Update pkgmanager_probe.sh

--- Local_Admin/pkgmanager_probe.sh
+++ Local_Admin/pkgmanager_probe.sh
@@ -1,11 +1,7 @@
-#!/usr/bin/env sh
-
 ## A function to determine package manager then update && upgrade
-#
-
 function pkgmgr_id() {
 # List possible package managers
-for PKGMGR in apt yum apk dnf emerge zypper pacman; do
+for PKGMGR in apt yum apk pkg brew pkgsrc dnf emerge zypper pacman; do
         test "$(command -v $PKGMGR)"
 done
 
@@ -14,7 +10,9 @@ case $PKGMGR in
         apt) UPDATE="update && sudo $PKGMGR upgrade -y";;
         yum) UPDATE="update";;
         apk) UPDATE="upgrade --no-cache";;
-        dnf) UPDATE="upgrade";;
+        dnf|pkg) UPDATE="upgrade";;
+        brew) UPDATE="update && $PKGMGR upgrade"
+        pkgsrc) PKGMGR="pkgin" && UPDATE="update && $PKGMGR upgrade";;
         emerge) UPDATE="--sync && $PKGMGR -[a]uDN @world";;
         zypper) UPDATE="refresh && $PKGMGR up";;
         pacman) UPDATE="-Scc --noconfirm && sudo $PKGMGR -Syyu --noconfirm";;
@@ -25,11 +23,9 @@ ACTION=$PKGMGR $UPDATE
 echo "Do you wish to update?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo $ACTION; break;;
+        Yes ) sudo $ACTION | less; break;;
         No ) exit;;
         *) echo "Please select an option."
-    esac
 done
 }
-
 pkgmgr_id
